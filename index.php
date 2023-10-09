c92c68bd9d449855e8a5b6ba8ccdcba47f533f7e
AlvaroVico <146710202+AlvaroVico@users.noreply.github.com>
2023-10-09 09:52:01 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+
+
 <html lang="es">
     <head>
         <title>Condicional</title>
